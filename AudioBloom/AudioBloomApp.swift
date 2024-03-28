2c0a25d9b8644c6a16fc76fb0eb78422fe3c7e3c
Angelina F <angelina.friz@gmail.com>
2024-03-28 19:46:19 +0200

    Initial Commit

---
+++ AudioBloom/AudioBloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioBloomApp.swift
+//  AudioBloom
+//
+//  Created by Angelina on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AudioBloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
